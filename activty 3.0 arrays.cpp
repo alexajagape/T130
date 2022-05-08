66d82803a3d2e4709871a1b9a4c5b00e7e6d6b63
alexa gin padernal <89246559+alexajagape@users.noreply.github.com>
2022-05-08 20:56:09 +0800

    Add files via upload

---
+++ activty 3.0 arrays.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int marks[10], i, n = 10 , sum = 0;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    
+    for(i=0; i<n; i++)
+    {
+    printf("Enter the number%d: ", i+1);
+    scanf("%d", &marks[i]);
+    
+    sum += marks[i];
+    }
+    printf("sum all array elements = %d", sum);
+    return 0;
+}  
